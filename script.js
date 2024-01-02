2b3a39bf4cad042a4b9737da14c9e79dc113664a
Karan Sayambar <karansayambar@gmail.com>
2024-01-02 13:05:12 +0530

    code is added

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let dishes = [
+
     {
         "name": "Veggie Delight",
         "imageSrc": "https://source.unsplash.com/random?veggies",
@@ -118,7 +119,234 @@ let dishes = [
         "type": "veg",
         "isLiked": false,
         "rating": 4.9
+    },
+    {
+        "name": "Pasta Primavera",
+        "imageSrc": "https://source.unsplash.com/random?pasta",
+        "time": "30 min",
+        "type": "veg",
+        "isLiked": false,
+        "rating": 4.4
+    },
+    {
+        "name": "Grilled Shrimp",
+        "imageSrc": "https://source.unsplash.com/random?shrimp",
+        "time": "40 min",
+        "type": "non-veg",
+        "isLiked": false,
+        "rating": 4.6
+    },
+    {
+        "name": "Vegetable Stir-Fry",
+        "imageSrc": "https://source.unsplash.com/random?stirfry",
+        "time": "25 min",
+        "type": "veg",
+        "isLiked": false,
+        "rating": 4.2
+    },
+    {
+        "name": "Beef Tacos",
+        "imageSrc": "https://source.unsplash.com/random?tacos",
+        "time": "35 min",
+        "type": "non-veg",
+        "isLiked": false,
+        "rating": 4.5
+    },
+    {
+        "name": "Caprese Salad",
+        "imageSrc": "https://source.unsplash.com/random?caprese",
+        "time": "15 min",
+        "type": "veg",
+        "isLiked": false,
+        "rating": 4.0
+    },
+    {
+        "name": "Pasta Carbonara",
+        "imageSrc": "https://source.unsplash.com/6p7BTlFUqO4",
+        "time": "30 min",
+        "type": "veg",
+        "isLiked": false,
+        "rating": 4.4
+    },
+    {
+        "name": "Grilled Salmon",
+        "imageSrc": "https://source.unsplash.com/L2Fi0JjU-Ts",
+        "time": "40 min",
+        "type": "non-veg",
+        "isLiked": false,
+        "rating": 4.6
+    },
+    {
+        "name": "Vegetarian Sushi",
+        "imageSrc": "https://source.unsplash.com/3yvPSg12Z98",
+        "time": "25 min",
+        "type": "veg",
+        "isLiked": false,
+        "rating": 4.2
+    },
+    {
+        "name": "Chicken Alfredo",
+        "imageSrc": "https://source.unsplash.com/SezVZcTbGW0",
+        "time": "35 min",
+        "type": "non-veg",
+        "isLiked": false,
+        "rating": 4.5
+    },
+    {
+        "name": "Caprese Salad",
+        "imageSrc": "https://source.unsplash.com/yITotKq8DCM",
+        "time": "15 min",
+        "type": "veg",
+        "isLiked": false,
+        "rating": 4.0
+    },
+    {
+        "name": "Honey Garlic Shrimp Stir-Fry",
+        "imageSrc": "https://source.unsplash.com/d6i7-6ovJS0",
+        "time": "50 min",
+        "type": "non-veg",
+        "isLiked": false,
+        "rating": 4.7
+    },
+    {
+        "name": "Eggplant Lasagna",
+        "imageSrc": "https://source.unsplash.com/NpHbviQ3_FI",
+        "time": "55 min",
+        "type": "veg",
+        "isLiked": false,
+        "rating": 4.8
+    },
+    {
+        "name": "Quinoa Bowl",
+        "imageSrc": "https://source.unsplash.com/G_Xc0jL2RbY",
+        "time": "20 min",
+        "type": "veg",
+        "isLiked": false,
+        "rating": 4.3
+    },
+    {
+        "name": "BBQ Pulled Chicken Sandwich",
+        "imageSrc": "https://source.unsplash.com/htN-qRw4znw",
+        "time": "60 min",
+        "type": "non-veg",
+        "isLiked": false,
+        "rating": 4.9
+    },
+    {
+        "name": "Vegan Buddha Bowl",
+        "imageSrc": "https://source.unsplash.com/s9CC2SKySJM",
+        "time": "45 min",
+        "type": "veg",
+        "isLiked": false,
+        "rating": 4.1
+    },
+
+    {
+        "name": "Eggplant Parmesan",
+        "imageSrc": "https://source.unsplash.com/random?eggplantparmesan",
+        "time": "50 min",
+        "type": "veg",
+        "isLiked": false,
+        "rating": 4.2
+    },
+    {
+        "name": "Lemon Garlic Butter Shrimp",
+        "imageSrc": "https://source.unsplash.com/random?lemonshrimp",
+        "time": "30 min",
+        "type": "non-veg",
+        "isLiked": false,
+        "rating": 4.6
+    },
+    {
+        "name": "Cauliflower Fried Rice",
+        "imageSrc": "https://source.unsplash.com/random?cauliflowerrice",
+        "time": "25 min",
+        "type": "veg",
+        "isLiked": false,
+        "rating": 4.3
+    },
+    {
+        "name": "Salmon Teriyaki",
+        "imageSrc": "https://source.unsplash.com/random?teriyakisalmon",
+        "time": "35 min",
+        "type": "non-veg",
+        "isLiked": false,
+        "rating": 4.7
+    },
+    {
+        "name": "Chickpea Curry",
+        "imageSrc": "https://source.unsplash.com/random?chickpeacurry",
+        "time": "40 min",
+        "type": "veg",
+        "isLiked": false,
+        "rating": 4.0
+    },
+    {
+        "name": "Baked Cod with Lemon and Herbs",
+        "imageSrc": "https://source.unsplash.com/random?bakedcod",
+        "time": "55 min",
+        "type": "non-veg",
+        "isLiked": false,
+        "rating": 4.5
+    },
+    {
+        "name": "Sweet Potato and Black Bean Burrito",
+        "imageSrc": "https://source.unsplash.com/random?burrito",
+        "time": "30 min",
+        "type": "veg",
+        "isLiked": false,
+        "rating": 4.1
+    },
+    {
+        "name": "Beef and Broccoli Stir-Fry",
+        "imageSrc": "https://source.unsplash.com/random?beefbroccoli",
+        "time": "40 min",
+        "type": "non-veg",
+        "isLiked": false,
+        "rating": 4.8
+    },
+    
+    {
+        "name": "Honey Glazed Chicken",
+        "imageSrc": "https://source.unsplash.com/random?honeychicken",
+        "time": "50 min",
+        "type": "non-veg",
+        "isLiked": false,
+        "rating": 4.7
+    },
+    {
+        "name": "Spinach and Feta Stuffed Chicken Breast",
+        "imageSrc": "https://source.unsplash.com/random?stuffedchicken",
+        "time": "55 min",
+        "type": "non-veg",
+        "isLiked": false,
+        "rating": 4.8
+    },
+    {
+        "name": "Quinoa Salad",
+        "imageSrc": "https://source.unsplash.com/random?quinoasalad",
+        "time": "20 min",
+        "type": "veg",
+        "isLiked": false,
+        "rating": 4.3
+    },
+    {
+        "name": "BBQ Pulled Pork Sandwich",
+        "imageSrc": "https://source.unsplash.com/random?pulledpork",
+        "time": "60 min",
+        "type": "non-veg",
+        "isLiked": false,
+        "rating": 4.9
+    },
+    {
+        "name": "Vegan Buddha Bowl",
+        "imageSrc": "https://source.unsplash.com/random?buddhabowl",
+        "time": "45 min",
+        "type": "veg",
+        "isLiked": false,
+        "rating": 4.1
     }
+
 ]
 
 
@@ -127,33 +355,43 @@ document.addEventListener("DOMContentLoaded", function () {
     const rate1Checkbox = document.getElementById('rate1');
     const rate2Checkbox = document.getElementById('rate2');
 
-    function showRecipes(filterType, ratingFilter) {
+    function showRecipes(filterType, ratingFilter, dishSearch) {
         menuContainer.innerHTML = '';
 
         dishes.forEach(dish => {
             if ((!filterType || dish.type === filterType) && (!ratingFilter || (ratingFilter === '4+' && dish.rating >= 4) || (ratingFilter === 'below4' && dish.rating < 4))) {
                 const menuItem = document.createElement('div');
                 menuItem.innerHTML = `
-                    <img src="${dish.imageSrc}" alt="${dish.name}">
-                    <div>
-                        <p>${dish.type}</p>
-                        <span class="menu-details">
-                            <h3>${dish.name}</h3>
-                            <p>${dish.rating}</p>
+                <img src="${dish.imageSrc}" alt="${dish.name}">
+                <div class="dish-info"> <!-- Added a class for styling -->
+                    <h5>${dish.type}</h5>
+                    <span class="menu-details">
+                        <h2>${dish.name}</h2>
+                        <p>${dish.rating} <i class="fa-solid fa-star"></i></p>
+                    </span>
+                    <span>
+                        <h4 class="dishtime">${dish.time}</h4>
+                        <span  class="likemsg">
+                        <i class="far fa-heart ${dish.isLiked ? 'liked' : ''}" data-dish="${dish.name}"></i>
+                        <i class="far fa-comment"></i>
                         </span>
-                        <span>
-                            <h3>${dish.time}</h3>
-                            <i class="far fa-heart ${dish.isLiked ? 'liked' : ''}" data-dish="${dish.name}"></i>
-                            <i class="far fa-comment"></i>
-                        </span>
-                    </div>
+                    </span>
+                </div>
                 `;
 
                 menuContainer.appendChild(menuItem);
 
+
+
+
                 const heartIcon = menuItem.querySelector('.fa-heart');
                 heartIcon.addEventListener('click', function () {
                     toggleLike(dish.name);
+                });
+
+                const spanElements = menuItem.querySelectorAll('span');
+                spanElements.forEach(span => {
+                    span.style.cssText = "display: flex; align-items: center; justify-content: space-between;";
                 });
             }
         });
@@ -163,11 +401,13 @@ document.addEventListener("DOMContentLoaded", function () {
         const dish = dishes.find(item => item.name === dishName);
         if (dish) {
             dish.isLiked = !dish.isLiked;
-
             const heartIcon = document.querySelector(`.fa-heart[data-dish="${dish.name}"]`);
             heartIcon.classList.toggle('liked', dish.isLiked);
         }
+        
     }
+
+
 
     function showRecipesByRating() {
         const ratingFilter = rate1Checkbox.checked ? '4+' : (rate2Checkbox.checked ? 'below4' : null);
@@ -201,7 +441,7 @@ document.addEventListener("DOMContentLoaded", function () {
     const navList = document.querySelector('.nav-list');
 
     homeIcon.addEventListener('click', function () {
-        navList.style.display = (navList.style.display === 'none' || navList.style.display === '') ? 'block' : 'none';
+        navList.style.display = (navList.style.display === 'block' || navList.style.display === '') ? 'block' : 'none';
         navList.style.flexDirection = "column";
     });
 });
